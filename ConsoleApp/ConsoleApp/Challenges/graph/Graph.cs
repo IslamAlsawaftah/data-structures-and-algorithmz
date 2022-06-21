1a10454296b9c445b025d236f6dc32cd45d6e689
IslamAlsawaftah <islamalsawaftah99@gmail.com>
2022-06-21 11:27:58 +0300

    graph implementation

---
+++ ConsoleApp/ConsoleApp/Challenges/graph/Graph.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.Challenges.graph
+{
+    public class Graph
+    {
+        private List<Vertex> vertices;
+        private bool isWeighted;
+        private bool isDirected;
+        public Graph(bool inputIsWeighted, bool inputIsDirected)
+        {
+            this.vertices = new List<Vertex>();
+            this.isWeighted = inputIsWeighted;
+            this.isDirected = inputIsDirected;
+        }
+        public Vertex AddNode(int val)
+        {
+            Vertex newvertex = new Vertex(val);
+            vertices.Add(newvertex);
+            return newvertex;
+        }
+        public void AddEdge(Vertex vertex1, Vertex vertex2, int weight)
+        {
+            if (!this.isWeighted)
+            {
+                weight = (int)(int?)null;
+            }
+            vertex1.addEdge(vertex2, weight);
+            if (!this.isDirected)
+            {
+                vertex2.addEdge(vertex1, weight);
+            }
+        }
+        public List<Vertex> GetNodes()
+        {
+            if (vertices.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return vertices;
+
+            }
+        }
+        public List<Edge> GetNeighbors(Vertex node)
+        {
+            return vertices.Find(v => v.value == node.value).edges;
+        }
+        public int Size()
+        {
+            return vertices.Count;
+        }
+        
+        public void PrintGraph(List<int>[] adj, int V)
+        {
+            for (int v = 0; v < V; ++v)
+            {
+                Console.Write("vertex " + v + " ");
+                foreach (int x in adj[v])
+                    Console.Write("-> " + x);
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Challenges/graph/README.md
@@ -0,0 +1,72 @@
+﻿# Graphs
+
+A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.
+
+A Graph consists of a finite set of vertices(or nodes) and set of Edges which connect a pair of nodes.
+
+# Challenge
+
+Graph implementation with folloing methods : 
+
+* AddNode 
+
+* AddEdge
+
+* GetNodes
+
+* GetNeighbors
+
+* Size
+
+# Approach & Efficiency
+
+| Method      | Time Complexity | Space Complexity
+| ----------- | ----------- | ----------- |
+| AddNode     |    O(1)   | O(1) |
+| AddEdge     |    O(1)   | O(1) |
+| GetNodes    |    O(n)   | O(n) |
+| GetNeighbors|    O(n)   | O(1) |
+| Size        |    O(1)   | O(1) |
+
+# API
+
+
+1- add node
+
+Arguments: value
+
+Returns: The added node
+
+Add a node to the graph
+
+2- add edge
+
+Arguments: 2 nodes to be connected by the edge, weight (optional)
+
+Returns: nothing
+
+Adds a new edge between two nodes in the graph
+
+If specified, assign a weight to the edge
+
+Both nodes should already be in the Graph
+
+3- get nodes
+
+Arguments: none
+
+Returns all of the nodes in the graph as a collection (set, list, or similar)
+
+4- get neighbors
+
+Arguments: node
+
+Returns a collection of edges connected to the given node
+
+Include the weight of the connection in the returned collection
+
+5- size
+
+Arguments: none
+
+Returns the total number of nodes in the graph
